0711c816143b9a58d51b2e126d69212df90a0062
Surapat Mekvanich <titleyugio@gmail.com>
2023-01-11 10:33:32 -0800

    commit tree 443aeedc4f7ff473796c4de6efa3a2b17fdd9215

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
 # your code goes here
+require 'pry'
+
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name # same as @person_name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def return_self
+        self
+    end
+
+
+end
+
+
+
+
+matt = Person.new("Matt Eva")
+ryan = Person.new("Ryan Sullivan")
+
+matt.return_self
+
+
+
+# binding.pry
+
